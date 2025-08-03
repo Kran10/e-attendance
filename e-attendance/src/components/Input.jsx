8411a11f7f0fac76b6323259522e543d79555e0d
Kiran <kirankunwar99@gmail.com>
2025-08-03 16:27:46 +0545

    Register UI updated, including password match

--- e-attendance/src/components/Input.jsx
+++ e-attendance/src/components/Input.jsx
@@ -1,15 +1,20 @@
 import'./Input.css';
 
-function Input({type='text', name, placeholder, value, onChange}){
+function Input({type='text', name, error,  placeholder, repassword, value, onChange, ...rest}){
     return(
+        <div style={{marginBottom: '10px'}}>
         <input 
         name={name}
         className="custom-input" 
         type={type} 
         placeholder={placeholder} 
         value={value} 
+        repassword={repassword}
         onChange={onChange}
+        {...rest}
         />
+        {error && <small style={{color: 'red'}}>{error}</small>}
+        </div>
     );
 }
 export default Input;
--- e-attendance/src/components/Register.jsx
+++ e-attendance/src/components/Register.jsx
@@ -1,31 +1,53 @@
-import {useState} from 'react';
+import React, {useState} from 'react';
+import {useForm} from 'react-hook-form';
+import axios from 'axios';
 import Input from './Input';
+import { Link } from 'react-router-dom';
+
 
 function Register(){
+    const{
+        register,
+        handleSubmit,
+        formState:{errors},
+        trigger,
+        watch
+    }=useForm();
+
+    const password=watch('password');
+    const confirmPassword=watch("repassword");
+    
+    const onSubmit=async (data)=>{
+        if(data.password!==data.repassword){
+        }
+        try{
+            const res=await axios.post('http://localhost:8080/api/users/register', data);
+            alert("Registration successful");
+        }catch(err){
+            alert("Error: "+err.response?.data||"Could not register");
+        };
+    }
     const[user, setUser]=useState({username: "", email: "", password:"" ,repassword:""});
     const [message, setMessage]=useState("");
 
     const handleChange=e=>{
         setUser({...user, [e.target.name]:e.target.value});
     };
-    const handleSubmit=async e=>{
-        e.preventDefault();
-        try{
-            const res=await axios.post('http://localhost:8080/api/users/register', user);
-            setMessage("Registration successful");
-        }catch(err){
-            setMessage("Error: "+err.response?.data||"Could not register");
-        }
-    };
+    
     return(
         
         <div className='register-container'>
             <div className="form-wrapper">
                 <h2>Register</h2>
-            <form className="register-form" onSubmit={handleSubmit}>
+            <form className="register-form" onSubmit={handleSubmit(onSubmit)}>
                 <Input 
                 name="username"
                 placeholder="Username"
+                {...register("username",{
+                    required: "Username is required",
+                    onblur:()=>trigger("username"),
+                })}
+                error={errors.username?.message}
                 value={user.username}
                 onChange={handleChange}/>
 
@@ -33,6 +55,15 @@ function Register(){
                 name="email"
                 type="email"
                 placeholder="Email"
+                {...register("email",{
+                    required: "Email is required",
+                    pattern:{
+                        value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                        message: "Invalid email format"
+                    },
+                    onBlur:()=>trigger("email"),
+                })}
+                error={errors.email?.message}
                 value={user.email}
                 onChange={handleChange}/>
 
@@ -40,20 +71,41 @@ function Register(){
                 name="password"
                 type="password"
                 placeholder="Password"
+                {...register("password",{
+                    required: "Password is required",
+                    minLength: {
+                        value:6,
+                        message: "Password must be at least 6 characters"
+                    },
+                    onBlur:()=>trigger("password")
+                })}
+                error={errors.password?.message}
                 value={user.password}
                 onChange={handleChange}/>
 
                 <Input 
                 name="repassword"
                 type="password"
+                {...register("repassword",{
+                    required: "Please confirm your password",
+                    onBlur: ()=>trigger("repassword"),
+                })}
+                error={errors.repassword?.message}
                 placeholder="Confirm Password"
-                value={user.repassowrd}
-                onChange={handleChange}/>
+                value={user.repassword}
+                onChange={handleChange}
+                />
+                {confirmPassword && password && (
+                <p style={{ color: password === confirmPassword ? 'darkgreen' : 'red', fontWeight: 'bold'}}>
+                    {password === confirmPassword ? 'Password is matched' : 'Passwords do not match'}
+                </p>
+                )}
 
                 <button className="custom-button">Submit</button>
+                <p className="info-message">Already have an account? <Link to="/login" style={{color: 'darkred', textDecoration: 'none', fontWeight:'bold'}}>Login</Link></p>
 
             </form>
-            
+
             {message && <p className="message">{message}</p>}
             </div>
         </div>
